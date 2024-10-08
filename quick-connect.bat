786edfb317e0e47aca94211e2761af684a526a57
Owen Jones <owen.jones@uwe.ac.uk>
2024-10-08 12:47:02 +0000

    don't need to delete directory, just clear it

--- quick-connect.bat
+++ quick-connect.bat
@@ -6,7 +6,6 @@ CALL az login
 
 IF EXIST "C:\Users\%username%\.ssh\csctcloud\" (
     del /q "C:\Users\%username%\.ssh\csctcloud"
-    rmdir "C:\Users\%username%\.ssh\csctcloud"
 )
 
 CALL az ssh config --ip csctcloud.uwe.ac.uk --file "C:\Users\%username%\.ssh\config" --keys-destination-folder "C:\Users\%username%\.ssh\csctcloud"
