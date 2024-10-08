9c9d3f819d72e22ff10a23db42526547c17e289d
Owen Jones <owen.jones@uwe.ac.uk>
2024-10-08 12:06:19 +0000

    Windows version of CLI appears to create directory for us, but we want to clear it out beforehand to avoid messages about overwritting

--- quick-connect.bat
+++ quick-connect.bat
@@ -1,6 +1,6 @@
 az config set core.login_experience_v2=off
 az login
 del /q "C:\Users\%username%\.ssh\csctcloud"
-mkdir "C:\Users\%username%\.ssh\csctcloud"
+rmdir "C:\Users\%username%\.ssh\csctcloud"
 az ssh config --ip csctcloud.uwe.ac.uk --file "C:\Users\%username%\.ssh\config" --keys-destination-folder "C:\Users\%username%\.ssh\csctcloud"
 code -n --remote ssh-remote+csctcloud.uwe.ac.uk
