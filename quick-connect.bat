e71a4bcb5ff57a31d4394f1ab580f7c093a73f14
Owen Jones <owen.jones@uwe.ac.uk>
2024-10-08 12:12:34 +0000

    this might actually work as a single file now?

--- quick-connect.bat
+++ quick-connect.bat
@@ -1,10 +1,11 @@
-az config set core.login_experience_v2=off
-az login
+@echo off
+call az config set core.login_experience_v2=off
+call az login
 
 if exist "C:\Users\%username%\.ssh\csctcloud\" (
     del /q "C:\Users\%username%\.ssh\csctcloud"
     rmdir "C:\Users\%username%\.ssh\csctcloud"
 )
 
-az ssh config --ip csctcloud.uwe.ac.uk --file "C:\Users\%username%\.ssh\config" --keys-destination-folder "C:\Users\%username%\.ssh\csctcloud"
-code -n --remote ssh-remote+csctcloud.uwe.ac.uk
+call az ssh config --ip csctcloud.uwe.ac.uk --file "C:\Users\%username%\.ssh\config" --keys-destination-folder "C:\Users\%username%\.ssh\csctcloud"
+call code -n --remote ssh-remote+csctcloud.uwe.ac.uk
