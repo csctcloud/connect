b16b39c0195764214d73170fd630dbaa7ee5f17b
Owen Jones <owen.jones@uwe.ac.uk>
2024-10-11 10:51:17 +0000

    .ssh directory needs to exists for config to be saved

--- quick-connect.bat
+++ quick-connect.bat
@@ -3,6 +3,10 @@
 CALL az config set core.login_experience_v2=off
 CALL az login
 
+IF NOT EXIST "C:\Users\%username%\.ssh\" (
+    mkdir "C:\Users\%username%\.ssh"
+)
+
 IF EXIST "C:\Users\%username%\.ssh\csctcloud\" (
     del /q "C:\Users\%username%\.ssh\csctcloud"
 )
