f438cde4ec889ceefa2a5d46b1d1e559c3a3395d
Owen Jones <Owen.Jones@uwe.ac.uk>
2024-11-14 16:12:48 +0000

    zip exe for release

--- connect/build.py
+++ connect/build.py
@@ -1,10 +1,14 @@
 import PyInstaller.__main__
+import shutil
+
+from src.connect import __VERSION__
 
 PyInstaller.__main__.run(
     [
         "--onefile",
         "--icon=src/icon/csctcloud.ico",
-        "--distpath=.",
         "src/connect.py",
     ]
 )
+
+shutil.make_archive(f"connect_{__VERSION__}", "zip", "dist")
