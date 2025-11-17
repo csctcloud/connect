7d2dc83d60a326f5e33f487e217c27796e5ff9d6
Owen Jones <owen@owenjones.net>
2025-11-17 17:51:35 +0000

    scrap the old scripts

--- old/quick-connect.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-az config set core.login_experience_v2=off
-az login
-rm -rf ~/.ssh/csctcloud
-mkdir -p ~/.ssh/csctcloud
-az ssh config --ip csctcloud.uwe.ac.uk --file ~/.ssh/config --keys-destination-folder ~/.ssh/csctcloud
-code -n --remote ssh-remote+csctcloud.uwe.ac.uk
